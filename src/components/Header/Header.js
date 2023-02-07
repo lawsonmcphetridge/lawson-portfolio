8504392478048bafa0eb5bcfd99be33d8cd44b4c
Lawson <lawsonmcphetridge2020@gmail.com>
2023-02-07 13:05:30 -0800

    start to portfolio

---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+  return (
+    <div className='main-header'>Portfolio resume about contact</div>
+  )
+}
---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Main.css'
+
+export default function Main() {
+  return (
+    <main className='main-content'>
+      <div className='name'>lawson mcphetridge</div>
+      <div className='contact'>contact</div>
+    </main>
+  );
+}
