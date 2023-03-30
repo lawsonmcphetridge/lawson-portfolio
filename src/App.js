0e43dea7c4594714948607265b06531204ea91b2
Lawson <lawsonmcphetridge2020@gmail.com>
2023-03-30 10:00:32 -0700

    Header link properly changing url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Main from './components/Main/Main';
 import Header from './components/Header/Header';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className='App'>
-      <Header></Header>
-      
-      <Main></Main>
+        <Header></Header>
+      <Routes>
+
+        <Route path='/' Component={Main}/>
+
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Header.css'
+import { NavLink } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div className='main-header'>
-      <div>About</div>
-      <div>Resume</div>
-      <div>Contact</div>
+      <NavLink to='/'>About</NavLink>
+      <NavLink to='/resume'>Resume</NavLink>
+      <NavLink to='/contact'>Contact</NavLink>
     </div>
   );
 }
